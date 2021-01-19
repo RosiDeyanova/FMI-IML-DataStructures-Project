b70919c287d3bb62c0f7aa32abcffb482ff820ae
RosiDeyanova <34300738+RosiDeyanova@users.noreply.github.com>
2021-01-20 01:28:23 +0200

    last buggy version

--- Parser.h
+++ Parser.h
@@ -13,7 +13,7 @@ private:
     stack<string>numbers;
     bool lastCommandOpenning;
     string input;
-    void calculate(const string& temp,int isThereError);
+    void calculate(const string& temp);
    
 public:
     Parser();
